99aba9b0162ab8369168e6caaa6d9fb8dcb0f805
Alex203maina <alex203maina@gmail.com>
2024-11-19 23:30:25 +0300

    updated database

--- dropu/dropu/urls.py
+++ dropu/dropu/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Add static files to the URL configuration
+from django.conf.urls.static import static
+
+# Import the media files configuration
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),  # Include the app's URL configuration
-    path('', include('app.urls')), ]
+    path('', include('app.urls')),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
